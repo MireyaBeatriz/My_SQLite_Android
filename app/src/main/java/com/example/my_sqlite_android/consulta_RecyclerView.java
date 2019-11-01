61fd691a4684b9e074f2c959b3658a194d811b94
MireyaBeatriz <56738133+MireyaBeatriz@users.noreply.github.com>
2019-10-31 22:10:50 -0600

    mi otro aporte

--- app/src/main/java/com/example/my_sqlite_android/consulta_RecyclerView.java
+++ app/src/main/java/com/example/my_sqlite_android/consulta_RecyclerView.java
@@ -4,51 +4,87 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class consulta_RecyclerView extends AppCompatActivity {
 
+    private static final String URL = "http://mjgl.com.sv/mysqlcrud/Api.php";
+    List<Productos> productosList;
+    RecyclerView recyclerView;
+
+    ProductsAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_consultar_recycleview);
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        productosList = new ArrayList<>();
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+        loadProductos();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        private void loadProductos(){
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, URL,
+                    new Response.Listener<String>() {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+                        public void onResponse(String response) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                            //Toast.makeText(Consulta_RecyclerView.this, ""+response, Toast.LENGTH_SHORT).show();
+
+                            try {
+                                JSONArray array = new JSONArray(response);
+                                int totalEncontrados = array.length();
+                                Toast.makeText(consulta_RecyclerView.this, "Total: "+totalEncontrados, Toast.LENGTH_SHORT).show();
+
+                                for (int i = 0; i < array.length(); i++) {
+
+                                    JSONObject articulosObject = array.getJSONObject(i);
+
+
+                                    productosList.add(new Productos(
+                                            articulosObject.getInt("codigo"),
+                                            articulosObject.getString("descripcion"),
+                                            articulosObject.getDouble("precio"),
+                                            articulosObject.getString("imagen")
+                                    ));
+                                }
+
+                                adapter = new ProductsAdapter(consulta_RecyclerView.this, productosList);
+                                recyclerView.setAdapter(adapter);
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(consulta_RecyclerView.this, "Error. Compruebe su acceso a Internet.", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            //Volley.newRequestQueue(this).add(stringRequest);
+            // MySingleton.getInstance(this).addToRequestQueue(stringRequest);
+            MySingleton.getInstance(consulta_RecyclerView.this).addToRequestQueue(stringRequest);
         }
 
-        return super.onOptionsItemSelected(item);
+
+
+
     }
-}
